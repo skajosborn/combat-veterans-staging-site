785a11ea582e3812c907070b4bb128979daca6b7
skajosborn <saraosborn29@yahoo.com>
2026-02-17 18:08:43 -0500

    fixing space under nav on mission pg

--- app/mission/page.tsx
+++ app/mission/page.tsx
@@ -2,7 +2,7 @@ import Mission from '@/components/Mission'
 
 export default function MissionPage() {
   return (
-    <main className="pt-20">
+    <main className="bg-[#0a0e27] pt-14 sm:pt-16">
       <Mission />
     </main>
   )
