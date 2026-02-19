ce31dd6ac9f6cc873f85e1474ee66be91fa08ac8
skajosborn <saraosborn29@yahoo.com>
2026-02-19 16:11:12 -0500

    added icon buttons for nav links and fixed build

--- components/NavLinkButton.tsx
+++ components/NavLinkButton.tsx
@@ -7,14 +7,16 @@ interface NavLinkButtonProps {
   subtitle: string;
   icon: React.ReactNode;
   type?: 'default' | 'donate';
+  className?: string;
 }
 
-const NavLinkButton: React.FC<NavLinkButtonProps> = ({ href, title, subtitle, icon, type = 'default' }) => {
+const NavLinkButton: React.FC<NavLinkButtonProps> = ({ href, title, subtitle, icon, type = 'default', className }) => {
   const isDonate = type === 'donate';
   return (
     <Link href={href} className={`relative flex flex-col items-center justify-center px-6 py-4 min-h-[180px] min-w-[180px] bg-black/50 border border-gray-700 transition-all duration-300 
       ${isDonate ? 'hover:shadow-white-glow' : 'hover:brightness-125 hover:border-gray-600 hover:shadow-white-glow'}
       group
+      ${className ?? ''}
     `}>
       <div className={`relative z-20 text-5xl text-white`}>
         {icon}
